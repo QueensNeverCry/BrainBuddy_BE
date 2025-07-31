b5d7f37e5a14c99e52882cfeb4a9d10193e40255
SUN-RAT <kimcode630@gmail.com>
2025-07-31 15:13:05 +0900

    chore : Removed trash.

--- CodeSet__/scripts/install_requirements.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Python 3.12.11
-
-python3.12 -m pip install --upgrade pip
-python3.12 -m pip install -r requirements.txt
--- CodeSet__/src/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from src.core.middleware import PreemptiveTokenRefreshMiddleware
-
-app = FastAPI()
-
-# 미들웨어 등록
-app.add_middleware(PreemptiveTokenRefreshMiddleware)
-
-# 아래에 API 라우터 등록 등...
