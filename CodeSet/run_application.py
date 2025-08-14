45c4e001355943f2ad59f6b800b7093edb6a1394
SUN-RAT <kimcode630@gmail.com>
2025-08-14 16:57:28 +0900

    fix(run-file): change code to run each server on three different ports
    - Adjusted server run configuration for separate WAS, WS, and static file servers.
    - Ensured each service listens on its own dedicated port.

--- CodeSet/run_application.py
+++ CodeSet/run_application.py
@@ -13,10 +13,26 @@ def RunHTTPS():
                 ssl_keyfile="../Test/SSL/dev.key",
                 ssl_certfile="../Test/SSL/cert.pem")
 
+def RunHTTP_1():
+    uvicorn.run("Application.main:app", host="0.0.0.0", port=9000)
+def RunHTTP_2():
+    uvicorn.run("Application.main:app", host="0.0.0.0", port=9001)
+def RunHTTP_3():
+    uvicorn.run("Application.main:app", host="0.0.0.0", port=9002)
+
 if __name__ == "__main__":
-    p1 = multiprocessing.Process(target=RunHTTP)
-    p2 = multiprocessing.Process(target=RunHTTPS)
+    # p1 = multiprocessing.Process(target=RunHTTP)
+    # p2 = multiprocessing.Process(target=RunHTTPS)
+    # p1.start()
+    # p2.start()
+    # p1.join()
+    # p2.join()
+    p1 = multiprocessing.Process(target=RunHTTP_1)
+    p2 = multiprocessing.Process(target=RunHTTP_2)
+    p3 = multiprocessing.Process(target=RunHTTP_3)
     p1.start()
     p2.start()
+    p3.start()
     p1.join()
     p2.join()
+    p3.join()
--- CodeSet/run_websocket.py
+++ CodeSet/run_websocket.py
@@ -1,8 +1,25 @@
+import multiprocessing
 import uvicorn
 
+def RunWS_1():
+    uvicorn.run("WebSocket.main:ws_app", host="0.0.0.0", port=9003)
+def RunWS_2():
+    uvicorn.run("WebSocket.main:ws_app", host="0.0.0.0", port=9004)
+def RunWS_3():
+    uvicorn.run("WebSocket.main:ws_app", host="0.0.0.0", port=9005)
+
 if __name__ == "__main__":
-    uvicorn.run("WebSocket.main:ws_app",
-                host="0.0.0.0",
-                port=9001, # Host 서버 구현시, 9001 로 변경할 것
-                ssl_keyfile="../Test/SSL/dev.key",
-                ssl_certfile="../Test/SSL/cert.pem")
+    # uvicorn.run("WebSocket.main:ws_app",
+    #             host="0.0.0.0",
+    #             port=9001, # Host 서버 구현시, 9001 로 변경할 것
+    #             ssl_keyfile="../Test/SSL/dev.key",
+    #             ssl_certfile="../Test/SSL/cert.pem")
+    p1 = multiprocessing.Process(target=RunWS_1)
+    p2 = multiprocessing.Process(target=RunWS_2)
+    p3 = multiprocessing.Process(target=RunWS_3)
+    p1.start()
+    p2.start()
+    p3.start()
+    p1.join()
+    p2.join()
+    p3.join()
